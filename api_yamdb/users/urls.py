b1101baa34173ca95414a29d0bb142b95dbf5c39
Игорь Бобров <maka.imura26@yandex.ru>
2023-03-31 11:44:04 +0300

    Almost done

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path
-from .views import signup, get_jwt_token
-# from rest_framework_simplejwt.views import (
-#     TokenObtainPairView,
-#     TokenRefreshView,
-# )
+from django.urls import include, path
+from rest_framework.routers import SimpleRouter
+
+from .views import UserViewSet, get_jwt_token, signup, user_me
+
+router = SimpleRouter()
+router.register('', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('v1/auth/signup/', signup),
-    path('v1/auth/token/', get_jwt_token)
+    path('signup/', signup),
+    path('token/', get_jwt_token),
+    path('me/', user_me),
+    path('', include(router.urls))
 ]
