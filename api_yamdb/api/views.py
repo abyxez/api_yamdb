87a8ab9eb05d3aadcf43de8ed97b7cebe6e57c9b
Костя Макбуков <agent-007-98@list.ru>
2023-04-02 19:57:39 +0300

    CSV final

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -5,10 +5,8 @@ from rest_framework.filters import SearchFilter
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import (LimitOffsetPagination,
                                        PageNumberPagination)
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from reviews.models import Category, Comment, Genre, Review, Title
-from users.models import User
-from .permissions import IsAdminOrReadOnly, IsModeratorOrReadOnly, IsAuthorOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsModeratorOrReadOnly
 
 from .serializers import (CategorySerializer, CommentSerializer,
                           GenreSerializer, ReviewSerializer, TitleSerializer)
@@ -47,7 +45,7 @@ class TitleViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     pagination_class = (LimitOffsetPagination, )
-    permission_classes = (IsAuthorOrReadOnly, IsModeratorOrReadOnly )
+    permission_classes = (IsModeratorOrReadOnly, )
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
@@ -61,7 +59,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     pagination_class = (LimitOffsetPagination, )
-    permission_classes = (IsAuthorOrReadOnly, IsModeratorOrReadOnly )
+    permission_classes = (IsModeratorOrReadOnly, )
 
     def get_queryset(self):
         review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
